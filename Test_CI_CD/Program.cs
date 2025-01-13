2883eb583b8df087f4aebbe371626d37a34c2687
Patrik Grabner <patrik.grabner@outlook.at>
2025-01-13 08:54:12 +0100

    Version 1.0

---
+++ Test_CI_CD/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_CI_CD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo Welt!");
+            Console.WriteLine("Version: 1.0");
+        }
+    }
+}
